0315c2247511d7b38bf73ae6e44029716f2ef725
myusercloud <lucasharrizon@gmail.com>
2026-01-24 18:38:07 +0300

    backend commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,12 @@ import authRoutes from "./routes/auth.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
